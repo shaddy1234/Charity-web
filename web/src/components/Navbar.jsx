7019ac9e42723c17d699b1874467d343a99eef9d
Shadrack Kimaau <musyokishadrach1@gmail.com>
2024-01-26 11:33:37 +0300

    Initial commit

---
+++ web/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './styles.css'
+
+function Navbar() {
+  return (
+    <div>
+        <nav>
+            <ul>
+                <li>About</li>
+                <li>Our Work</li>
+                <li>Contact</li>
+                <li>Projects</li>
+        </ul>
+        <button>DONATE</button>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
